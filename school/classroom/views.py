283e5f5d0ca7b288f903911e821099bc2717379d
tancoder <tanmaygupta2408@gmail.com>
2023-03-05 00:13:51 +0530

    formView classbased view added

--- school/classroom/views.py
+++ school/classroom/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
+from classroom.forms import ContactForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 # def home_view(request):
@@ -10,3 +12,15 @@ class HomeView(TemplateView):
 
 class ThankYouView(TemplateView):
     template_name = 'classroom/thank_you.html' 
+
+class ContactFormView(FormView):
+    form_class = ContactForm
+    template_name = 'classroom/contact.html'
+
+    # URL not a template.html
+    success_url = reverse_lazy("classroom:thank_you")
+
+    # what to do with form?
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
